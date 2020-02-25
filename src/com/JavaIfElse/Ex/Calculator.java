409401157ab45e7f691e97058474c1034e8c7731
IoanaRebrisorean <ioana.rebrisorean@yahoo.com>
2020-02-24 21:44:24 -0800

    none

--- src/com/JavaIfElse/Ex/Calculator.java
+++ src/com/JavaIfElse/Ex/Calculator.java
@@ -23,7 +23,41 @@ public class Calculator {
     public float div(int number1, int number2) {
         float div = (float) number1 / (float) number2;
         return div;
-
-
     }
+
+    public int add(int number1, int number2, int number3) {
+        int add = number1 + number2 + number3;
+        return add;
+    }
+
+    public float add(float number1, float number2) {
+        float add = number1 + number2;
+        return add;
+    }
+
+    public int add(int number1, int number2, int number3, int number4) {
+        int add = number1 + number2 + number3 + number4;
+        return add;
+    }
+
+    public int diff(int number1, int number2, int number3) {
+        int diff = number1 - number2 - number3;
+        return diff;
+    }
+
+    public float diff(float number1, float number2) {
+        float diff = number1 - number2;
+        return diff;
+    }
+
+    public float multiply(float number1, float number2) {
+        float multiply = number1 * number2;
+        return multiply;
+    }
+
+    public int multiply(int number1, int number2, int number3) {
+        int multiply = number1 * number2 * number3;
+        return multiply;
+    }
+
 }
--- src/com/JavaIfElse/Ex/Main.java
+++ src/com/JavaIfElse/Ex/Main.java
@@ -6,24 +6,34 @@ public class Main {
 
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
-//	calculator.add(5,7);
-//	calculator.diff(9,6);
-//	calculator.multiply(4,5);
-//	calculator.div(9,4);
-        System.out.println(" sum = " + calculator.add(4, 5) + " diff = " + calculator.diff(6, 9) + " prod = " + calculator.multiply(4, 6) + " div = " + calculator.div(9, 4));
+//	      calculator.add(5,7);
+//        calculator.diff(9,6);
+//	      calculator.multiply(4,5);
+//	      calculator.div(9,4);
+//        System.out.println(" sum = " + calculator.add(4, 5) + " diff = " + calculator.diff(6, 9) + " prod = " + calculator.multiply(4, 6) + " div = " + calculator.div(9, 4));
+
+
+//        System.out.println(calculator.multiply(3.4f,4.5f));
+//        System.out.println(calculator.multiply(3,4,5));
+//        System.out.println(calculator.add(4.5f,5.6f));
+//        System.out.println(calculator.add(3,4,5,6));
+//        System.out.println(calculator.diff(3.4f,4.5f));
+//        System.out.println(calculator.diff(7,6,5));
+
+
 
 
         LogicalOp logicalOp = new LogicalOp();
         //if-else
-        System.out.println(" biggest number is: " + logicalOp.checkBiggerNumber(8,5));
-        System.out.println(" Strings comparsion: " + logicalOp.comparison("FastTrackIT"));
-        System.out.println(" String and number: " + logicalOp.stringAndNumberComparison("FastTrackIT",   3));
-        System.out.println(logicalOp.amountOfSnow(6));
-        System.out.println(logicalOp.numberComparsion(3));
-        System.out.println(logicalOp.givenNumber(12));
-        System.out.println(" Is even number: " + logicalOp.isNumberEven(5));
-        System.out.println(" Is eligible to vote: " + logicalOp.isEligibleToVote(19));
-        System.out.println( " The biggest number is: " + logicalOp.biggestOf3Numbers(5,7,4));
+//        System.out.println(" biggest number is: " + logicalOp.checkBiggerNumber(8,5));
+//        System.out.println(" Strings comparsion: " + logicalOp.comparison("FastTrackIT"));
+//        System.out.println(" String and number: " + logicalOp.stringAndNumberComparison("FastTrackIT",   3));
+//        System.out.println(logicalOp.amountOfSnow(6));
+//        System.out.println(logicalOp.numberComparsion(3));
+//        System.out.println(logicalOp.givenNumber(12));
+//        System.out.println(" Is even number: " + logicalOp.isNumberEven(5));
+//        System.out.println(" Is eligible to vote: " + logicalOp.isEligibleToVote(19));
+//        System.out.println( " The biggest number is: " + logicalOp.biggestOf3Numbers(5,7,4));
 
         //for
 //        logicalOp.printNumbers(19);
@@ -48,6 +58,8 @@ public class Main {
 //        logicalOp.div7(14,35);
 //        logicalOp.fibonacci();
 //        logicalOp.cozzaLozzaWozza();
+
+        //arrays
 
 
     }
